c4916d99659c945d757ccea4bc466245eaa9cfea
Mikołaj Lubiak <lubiak@proton.me>
2023-05-28 10:43:34 +0200

    Revert "Added rate limiting"
    This reverts commit 3cc0de98b8f4b2263f7a7eec0e98ec0a5deef070.

--- upload.php
+++ upload.php
@@ -1,31 +1,11 @@
 <?php
-require 'vendor/autoload.php';
-use bandwidthThrottle\tokenBucket\Rate;
-use bandwidthThrottle\tokenBucket\TokenBucket;
-use bandwidthThrottle\tokenBucket\storage\FileStorage;
-
-$storage = new FileStorage(__DIR__ . "/emote.bucket");
-$rate	= new Rate(10, Rate::MINUTE);
-$bucket  = new TokenBucket(10, $rate, $storage);
-
-$lock = "emote.lock" ;
-if(!is_file($lock))
-{
-	$bucket->bootstrap(10);
-	touch($lock);
-}
-
-if (!$bucket->consume(1, $minutes)) {
-	http_response_code(429);
-	header(sprintf("Retry-After: %d", floor($minutes)));
-	exit();
-}
 
 $target_dir = "emotes/";
 $target_file = $target_dir . basename($_FILES["emote"]["name"]);
+$uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
 $required = array($_POST['emotename'], $target_file);
+
 foreach($required as $field) {
 	if (empty($field)) {
 		die(nl2br("\nYou need to fill all fields"));
@@ -40,31 +20,42 @@ if(isset($_POST["submit"])) {
 		$uploadOk = 1;
 	} else {
 		die(nl2br("\nFile is not an emote."));
+		$uploadOk = 0;
 	}
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
 	die(nl2br("\nSorry, emote with this name already exists."));
+	$uploadOk = 0;
 }
 
 // Check file size
 if ($_FILES["emote"]["size"] > 20000000) {
 	die(nl2br("\nSorry, your emote is too large. Max size emote is 20MB"));
+	$uploadOk = 0;
 }
 
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" && $imageFileType != "webp" && $imageFileType != "mp4" ) {
 	die(nl2br("\nSorry, only WEBP, MP4, JPG, JPEG, PNG & GIF emotes are allowed."));
+	$uploadOk = 0;
 }
 
-if (move_uploaded_file($_FILES["emote"]["tmp_name"], $target_file)) {
-	echo nl2br("\nThe file ". htmlspecialchars( basename( $_FILES["emote"]["name"])). " has been uploaded.");
-	echo '<br><br><img src="' . $target_file . '"><br>';
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+	die(nl2br("\nSorry, your emote was not uploaded."));
+// if everything is ok, try to upload file
 } else {
-	die(nl2br("\nSorry, there was an error uploading your file."));
+	if (move_uploaded_file($_FILES["emote"]["tmp_name"], $target_file)) {
+		echo nl2br("\nThe file ". htmlspecialchars( basename( $_FILES["emote"]["name"])). " has been uploaded.");
+		echo '<br><img src="' . $target_file . '">';
+	} else {
+		die(nl2br("\nSorry, there was an error uploading your file."));
+	}
 }
+
 
 $username = "root";
 $password = "passwd";
@@ -86,4 +77,5 @@ if ( $conn -> query( $sql ) === TRUE ) {
 }
 
 $conn -> close();
+
 ?>
