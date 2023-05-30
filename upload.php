15c8c728836fe69633bebd30a84183d9c608dbf8
Mikołaj Lubiak <lubiak@proton.me>
2023-05-30 21:01:39 +0200

    Big update Changed naming convention Optimized code Deleted unnesessary code Refactorized code

--- upload.php
+++ upload.php
@@ -1,66 +1,61 @@
 <?php
-$target_dir = "emotes/";
-$target_file = $target_dir . basename($_FILES["emote"]["name"]);
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+const MAX_FILE_SIZE = 5000000; # 5MB
 
-$required = array($_POST['emotename'], $target_file);
+const DB_HOSTNAME = "localhost";
+const DB_USERNAME = "root";
+const DB_PASSWORD = "passwd";
+const DB_NAME = "emotebot";
+
+const EMOTE_NAME = $_POST["emotename"];
+const TARGET_DIR = "emotes/";
+const FILE_EXTENSION = strtolower(pathinfo($_FILES["emote"]["name"]), PATHINFO_EXTENSION));
+const TARGET_FILE = TARGET_DIR . EMOTE_NAME . FILE_EXTENSION;
+const TMP_FILE = $_FILES["emote"]["tmp_name"];
+const FILE_SIZE = $_FILES["emote"]["size"];
+
+
+$required = array(EMOTE_NAME, TARGET_FILE);
 foreach($required as $field) {
 	if (empty($field)) {
-		die(nl2br("\nYou need to fill all fields"));
+		die(nl2br("\nYou need to fill all the fields."));
 	}
 }
 
 // Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-	$check = getimagesize($_FILES["emote"]["tmp_name"]);
-	if($check !== false) {
-		echo nl2br("\nFile is an emote - " . $check["mime"] . ".");
-	} else {
-		die(nl2br("\nFile is not an emote."));
-	}
+if($check == getimagesize(TMP_FILE)) {
+	die(nl2br("\nFile is not an image or gif."));
 }
 
 // Check if file already exists
-if (file_exists($target_file)) {
-	die(nl2br("\nSorry, emote file name already exists."));
+if (file_exists(TARGET_FILE)) {
+	die(nl2br("\nFile name already exists.")); // You should also configure the database so that the name and location fields are unique
 }
 
 // Check file size
-if ($_FILES["emote"]["size"] > 20000000) {
-	die(nl2br("\nSorry, your emote is too large. Max size emote is 20MB"));
+if (FILE_SIZE > MAX_FILE_SIZE) {
+	die(nl2br("\nFile is too large. Max file size is 5MB."));
 }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" && $imageFileType != "webp") {
-	die(nl2br("\nSorry, only WEBP, JPG, JPEG, PNG & GIF emotes are allowed."));
+if(FILE_EXNTENSION != "jpg" && FILE_EXNTENSION != "png" && FILE_EXNTENSION != "jpeg"
+&& FILE_EXNTENSION != "gif" && FILE_EXNTENSION != "webp") {
+	die(nl2br("\nOnly WEBP, JPG, JPEG, PNG & GIF extensions are allowed."));
 }
 
-if (move_uploaded_file($_FILES["emote"]["tmp_name"], $target_file)) {
-	echo nl2br("\nThe file ". htmlspecialchars( basename( $_FILES["emote"]["name"])). " has been uploaded.");
-	echo '<br><br><img src="' . $target_file . '"><br>';
+if (move_uploaded_file(TMP_FILE, TARGET_FILE)) {
+	echo nl2br("\nEmote has been uploaded.");
+	echo sprintf("<br><img src='%s'>", TARGET_FILE);
 } else {
-	die(nl2br("\nSorry, there was an error uploading your file."));
+	die(nl2br("\nThere was an error."));
 }
 
-$username = "root";
-$password = "passwd";
-$dbname = "emotebot";
+$conn = new mysqli(DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-$conn = new mysqli("localhost", $username, $password, $dbname);
-$sql = sprintf("INSERT INTO emotes
-		(name, location)
-		VALUES
-		('%s', '%s')",
-		$conn -> real_escape_string( $_POST['emotename'] ),
-		$conn -> real_escape_string( $target_file ),
-		);
-
-if ( $conn -> query( $sql ) === TRUE ) {
-	echo nl2br("\nNew emote created successfully");
-} else {
-	die(nl2br("\nError: " . $sql . "<br>" . $conn->error));
-}
+$conn->set_charset("utf8mb4");
+$stmt = $conn->prepare("INSERT INTO emotes (name, location) VALUES (?, ?)");
+$stmt->bind_param("ss", EMOTE_NAME, TARGET_FILE);
+$stmt->execute();
+$stmt->close();
 
 $conn -> close();
 ?>
