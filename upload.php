2c57cd3d3c5eea454b818b3a3323ccb88734b3da
Mikołaj Lubiak <lubiak@proton.me>
2023-06-23 16:24:07 +0200

    Changed the DB name

--- upload.php
+++ upload.php
@@ -7,7 +7,7 @@ const TARGET_DIR = "emotes/";
 const DB_HOSTNAME = "localhost";
 const DB_USERNAME = "root";
 const DB_PASSWORD = "passwd";
-const DB_NAME = "emotebot";
+const DB_NAME = "emote";
 
 define("EMOTE_NAME", $_POST["emotename"]);
 define("FILE_EXTENSION", strtolower(pathinfo($_FILES["emote"]["name"], PATHINFO_EXTENSION)));
